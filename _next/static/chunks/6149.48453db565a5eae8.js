b3c41ac23791fafed5bc0fdbdc3f75aa63195864
Parallel <llelparallel@gmail.com>
2022-04-19 21:25:37 +0700

    Updates

---
+++ _next/static/chunks/6149.48453db565a5eae8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6149],{56149:function(e,n,r){r.r(n);var u=r(85893),c=r(11163),s=r(13968),t=r(36044);n.default=function(){var e=(0,c.useRouter)().query.collectionAddress,n=(0,s.YD)(e);return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(t.Z,{collection:n})})}}}]);
